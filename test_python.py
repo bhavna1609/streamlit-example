349fe534a2faee1dd49456aa5300ad165c1ac883
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-08-31 12:27:30 +0530

    Create test_python.py

---
+++ test_python.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import time
+from snowflake.snowpark.context import get_active_session
+
+
+session = get_active_session()
+st.set_page_config(page_title='User', page_icon=':wave:')
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
+#status elements
+#st.snow()
+#st.balloons()
+#Title
+st.title('First :blue[Streamlit] web app :sunglasses:')
+st.text("")
+st.text("")
+
+st.sidebar.success('Welcome to Home Page :tada:')
+#Code block
+  #code = '''st.title('First :blue[Streamlit] web app :sunglasses:')'''
+  #st.code(code, language='python')
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+# Perform query.
+df = conn.query('SELECT top 10 * from SNOWFLAKE.ACCOUNT_USAGE.USERS;', ttl=600)
+st.map(df)
+
