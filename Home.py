399168b70c4a0ef8a7bb3d1cbdcc038b49a73c28
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-04 13:09:25 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,10 @@
-#
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Monitoring Dashboard! 👋")
+
+st.sidebar.success("Select a page below.")
