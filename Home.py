c5ffe03f51a7ccb7253ef3d05fe446749cebe732
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-07 13:10:54 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,5 +8,7 @@ st.set_page_config(
 st.write("# Welcome to Monitoring Dashboard! 👋")
 
 st.sidebar.success("Select a page above.")
+
+conn = st.experimental_connection('snowpark')
 df = conn.query('SELECT ROUND(SUM(CREDITS_USED),2) AS YTD_COMPUTE_CREDITS FROM SNOWFLAKE.ORGANIZATION_USAGE.WAREHOUSE_METERING_HISTORY;', ttl=600)
 st.metric(label="Compute Credits", value=df, delta=none)
