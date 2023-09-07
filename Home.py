86e1d77a0330fefa48265b2de9a032ad44597546
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-07 13:24:19 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,6 +11,24 @@ st.write("# Welcome to Monitoring Dashboard! 👋")
 st.sidebar.success("Select a page above.")
 
 conn = st.experimental_connection('snowpark')
-df = conn.query('SELECT ROUND(SUM(CREDITS_USED),2) AS YTD_COMPUTE_CREDITS FROM SNOWFLAKE.ORGANIZATION_USAGE.WAREHOUSE_METERING_HISTORY;', ttl=600)
-compute_credit = pd.to_numeric(df)
-st.metric(label="Compute Credits", value=compute_credit)
+#df = conn.query('SELECT ROUND(SUM(CREDITS_USED),2) AS YTD_COMPUTE_CREDITS FROM SNOWFLAKE.ORGANIZATION_USAGE.WAREHOUSE_METERING_HISTORY;', ttl=600)
+#compute_credit = pd.to_numeric(df)
+#st.metric(label="Compute Credits", value=compute_credit)
+# create three columns
+kpi1, kpi2, kpi3 = st.columns(3)
+
+# fill in those three columns with respective metrics or KPIs
+kpi1.metric(
+    label="Compute_Credits",
+    value="4.25"
+)
+
+kpi2.metric(
+    label="Accounts",
+    value="1"
+)
+
+kpi3.metric(
+    label="Warehouses",
+    value="1"
+)
