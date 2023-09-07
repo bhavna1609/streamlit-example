28e24440579176dc12b7aca80797f9ba870bdd6a
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-07 13:13:16 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,4 +11,5 @@ st.sidebar.success("Select a page above.")
 
 conn = st.experimental_connection('snowpark')
 df = conn.query('SELECT ROUND(SUM(CREDITS_USED),2) AS YTD_COMPUTE_CREDITS FROM SNOWFLAKE.ORGANIZATION_USAGE.WAREHOUSE_METERING_HISTORY;', ttl=600)
-st.metric(label="Compute Credits", value=df)
+compute_credit = to_numeric(df)
+st.metric(label="Compute Credits", value=compute_credit)
