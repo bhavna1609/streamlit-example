8344dff2e533aa82b05ef01af685b076eea7bc86
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-07 12:53:07 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,5 +8,5 @@ st.set_page_config(
 st.write("# Welcome to Monitoring Dashboard! 👋")
 
 st.sidebar.success("Select a page above.")
-df = conn.query('SELECT ROUND(SUM(CREDITS_USED),2) AS YTD_COMPUTE_CREDITS FROM SNOWFLAKE.ORGANIZATION_USAGE.WAREHOUSE_METERING_HISTORY WHERE WAREHOUSE_NAME<>'CLOUD_SERVICES_ONLY;')
+df = conn.query('SELECT ROUND(SUM(CREDITS_USED),2) AS YTD_COMPUTE_CREDITS FROM SNOWFLAKE.ORGANIZATION_USAGE.WAREHOUSE_METERING_HISTORY;')
 st.metric(label="Compute Credits", value=df, delta=none)
