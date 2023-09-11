60f02c9f201fca284c17254ae5c1c3aa5a4bb9d3
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-11 11:07:28 +0530

    Update 2_Performance_Monitoring.py

--- pages/2_Performance_Monitoring.py
+++ pages/2_Performance_Monitoring.py
@@ -111,7 +111,7 @@ df
 
 
 #df=px.data.tips()
-fig=px.bar(df,x='QUERY_TEXT',y='EXEC_TIME_IN_SECONDS', orientation='h')
+fig=px.bar(df,y='EXEC_TIME_IN_SECONDS', orientation='h')
 st.write(fig)
 
 #st.pyplot(df.plot.barh(stacked=True).figure)
