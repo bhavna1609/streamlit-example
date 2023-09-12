61f7e3150d51c4455239e5e6a4218bcddaf711dd
bhavna1609 <138441743+bhavna1609@users.noreply.github.com>
2023-09-12 11:33:30 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -58,7 +58,7 @@ with st.form("data_editor_form"):
 if submit_button:
     try:
         #Note the quote_identifiers argument for case insensitivity
-        session.write_pandas(edited, "ACCOUNT_INFO_TABLE", quote_identifiers=False)
+        session.write_pandas(edited, "ACCOUNT_INFO_TABLE",overwrite=True, quote_identifiers=False)
         st.toast("Budget updated successfully")
         time.sleep(5)
     except:
